e8052f6712def6909cd685b2a7bc52759c65adad
markusmatthys <61882054+markusmatthys@users.noreply.github.com>
2022-02-16 17:26:52 +0100

    Add files via upload

---
+++ DirectBeviorRating/Model/Pupil.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using SQLite;
+
+namespace DirectBeviorRating.Model
+{
+    public class Pupil
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
